3a092efaad4549a1209f3d07488a42155e5ea450
Kylie <notcherry@jinhakapply.com>
2025-02-04 11:12:02 +0900

    user service(cqrs포함)

--- src/users/entity/user.entity.ts
+++ src/users/entity/user.entity.ts
@@ -1,19 +1,19 @@
-import { Entity, Column, PrimaryColumn } from "typeorm";
+import { Column, Entity, PrimaryColumn } from 'typeorm';
 
 @Entity('User')
-export class User {
-    @PrimaryColumn()
-    id: string;
+export class UserEntity {
+  @PrimaryColumn()
+  id: string;
 
-    @Column({length: 30})
-    name: string;
-    
-    @Column({length: 60})
-    email: string;
+  @Column({ length: 30 })
+  name: string;
 
-    @Column({length: 30})
-    password: string;
+  @Column({ length: 60 })
+  email: string;
 
-    @Column({length: 60})
-    signupVerifyToken: string;
+  @Column({ length: 30 })
+  password: string;
+
+  @Column({ length: 60 })
+  signupVerifyToken: string;
 }
